98b44e8c8239d99806cdaa516708c3dd7abeafbb
John Fiewor <johnfiewor@gmail.com>
2021-04-27 17:31:20 +0100

    another commit

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-alert("Hello, you handsome angel");
-document.querySelector().innerHTML = "";
+// alert("Hello, you handsome angel");
+//document.querySelector().innerHTML = "";
+//let header = document.firstElementChild.lastElementChild.firstElementChild;
+//header = "Good Bye!";
