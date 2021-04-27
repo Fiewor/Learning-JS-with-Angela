168ebc3edada7ac96831181e3ea482d4a9ac9a50
John Fiewor <johnfiewor@gmail.com>
2021-04-27 15:46:38 +0100

    more JS snippets plus some DOM practice

---
+++ 99bottles.js
@@ -0,0 +1,8 @@
+while (count < 100) {
+    if (count > 0 ) {
+        console.log(count + "bottles of beer on the wall, " + count + "bottles of beer.\nIf one of those bottles should happen to fall," + number + " bottles of beer on the wall...")
+    } else {
+        console.log ("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall...");
+    }
+}
+count--; 
