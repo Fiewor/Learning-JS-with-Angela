4d2523ed394d8cb0132bb4234aafba684fd71884
John Fiewor <johnfiewor@gmail.com>
2021-04-28 14:47:44 +0100

    more DOM stuff

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 //document.querySelector().innerHTML = "";
 //let header = document.firstElementChild.lastElementChild.firstElementChild;
 //header = "Good Bye!";
+
+//broader
+// document.getElementsByClassName("btn")[0].style.background= "blue";
+// document.getElementsByTagName("li")[2].style.background = "red";
+// document.firstElementChild.lastElementChild.firstElementChild; 
+
+// more specific
+// document.querySelector("#list .item"); //first .item under #list
+// document.querySelectorAll("#list .item"); //all .item under #list
